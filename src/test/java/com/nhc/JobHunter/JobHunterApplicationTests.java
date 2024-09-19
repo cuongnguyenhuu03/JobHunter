746b077e07c291de9941518311b10a644dd412a0
CuongDepTrai <nguyenhuucuong111103@gmail.com>
2024-09-19 15:38:59 +0700

    first commit

---
+++ src/test/java/com/nhc/JobHunter/JobHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhc.JobHunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobHunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
